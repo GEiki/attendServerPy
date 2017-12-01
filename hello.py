92ff74da698bd0285d89956cbfd2fb3d62fae850
CHkymo <31071352+CHkymo@users.noreply.github.com>
2017-12-01 21:15:24 +0800

    no

--- hello.py
+++ hello.py
@@ -1,4 +1,31 @@
-print('hello')
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+
+class MyHttpHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            f = open(self.path[1:], 'r')
+            self.send_response(200)
+            self.send_header('Content-type', 'text')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+def main():
+    try:
+        server = HTTPServer(('', 80), MyHttpHandler)
+        print('Welcome to the machine...')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
+
+
+
 
 if __name__ == '__main__':
     print('ok')
---
+++ testFlask.py
@@ -0,0 +1,88 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+import SQLUtil
+import json
+
+app = Flask(__name__)
+
+# def deserializer(info):
+#     d = request.get_data()
+#     data = json.loads(d)
+#     return data
+
+@app.route('/')
+def hello_world():
+    pass
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+
+@app.route('/getuserinfo', methods=['GET', 'POST'])
+def getuserinfo():
+    if request.method == 'POST':
+        d = request.get_data()
+        data = json.loads(d)
+        try:
+            result = SQLUtil.getUserinfo(dict['userid'], dict['password'])
+            dataformat = json.dumps(result)
+            return jsonify(dataformat)
+        except:
+            return 'id not exits'
+
+@app.route('/insertuserinfo', methods=['GET', 'POST'])
+def insertuserinfo():
+    if request.method == 'POST':
+        d = request.get_data()
+        data = json.loads(d)
+        return SQLUtil.insertUserInfo(data)
+
+@app.route('/getinfo', methods=['GET', 'POST'])
+def getinfo():
+    if request.method == 'POST':
+        d = request.get_data()
+        data = json.loads(d)
+        result = SQLUtil.getInfo(data['id'], data['identity'])
+        if reuslt == 0:
+            return 0
+        else:
+            dataformat = json.dumps(result)
+            return jsonify(dataformat)
+
+@app.route('/getcourseinfo', methods=['GET', 'POST'])
+def getcourseinfo():
+    if request.method == 'POST':
+        d = request.get_data()
+        data = json.loads(d)
+        result = SQLUtil.getCourseInfo(data['id'], data['identity'])
+        if result == 0:
+            return 0
+        else:
+            dataformat = json.dumps(result)
+            return jsonify(dataformat)
+
+@app.route('/getAttendanceInfo', method=['GET', 'POST'])
+def getattendanceinfo():
+    if request.method == 'POST':
+        d = request.get_data()
+        data = json.loads(d)
+        result = SQLUtil.getAttendanceInfo(data['id'], data['identity'])
+        if result == 0:
+            return 0
+        else:
+            dataformat = json.dumps(result)
+            return jsonify(dataformat)
+
+@app.route('/addattendanceinfo', method=['GET', 'POST'])
+def addattendanceinfo():
+    if request.method == 'POST':
+        d = request.get_data()
+        data = json.loads(d)
+        return  SQLUtil.addAttendanceInfo()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
